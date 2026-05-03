b5aab28b16bb62ca20154df9063aeafd8ae98886
Nightmare <datalab2026@gmail.com>
2026-05-03 20:13:46 +0300

    Update role and channel IDs in Alt_Dector.py

--- commands/Alt_Dector.py
+++ commands/Alt_Dector.py
@@ -17,9 +17,9 @@ class AltDetector(commands.Cog):
         self.CLOUD_API = config.ROBLOX_API_KEY
         self.headers = {"x-api-key": self.CLOUD_API}
         
-        self.ROLE_ID = 593902054
-        self.REPORT_CHANNEL_ID = 1494677345263681668
-        self.ERROR_CHANNEL_ID = 1494676707955835011
+        self.ROLE_ID = 82396916
+        self.REPORT_CHANNEL_ID = 1500544973010112632
+        self.ERROR_CHANNEL_ID = 1500544973010112632
 
         self.STARTER_ASSET_IDS = [
             62724852, 144076436, 144076512, 10638267973, 10647852134, 
